beb17bd21a63facd370e7a683eb7f021c9616683
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-04-02 16:26:00 +0900

    react_start

---
+++ 2. 실무 프로젝트(P-1)/exam01/src/App.js
@@ -0,0 +1,8 @@
+import MyComponents from './components/MyComponent';
+
+
+const App = () => {
+  return <MyComponents name = {15}num = {3} />
+};
+
+export default App;
---
+++ 2. 실무 프로젝트(P-1)/exam01/src/components/MyComponent.js
@@ -0,0 +1,22 @@
+import propTypes  from "prop-types";
+
+const MyComponent = ({name, num}) => {
+
+    return (
+    <>
+     <div>안녕하세요!, 제 이름은 {name}입니다.</div>
+     <div>좋아하는 숫자는 {num}입니다.</div>
+    </>
+    );
+};
+
+MyComponent.defaultProps = {
+    name: "기본 이름",
+};
+
+MyComponent.propTypes = {
+    name: propTypes.string,
+    num: propTypes.number.isRequired
+};
+
+export default MyComponent;
