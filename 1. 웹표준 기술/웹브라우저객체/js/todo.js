ce8ddc4e35592d92e6b74b6c1626dfc42e034483
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-04-09 17:50:05 +0900

    js_todo_list

--- 1. 웹표준 기술/웹브라우저객체/js/todo.js
+++ 1. 웹표준 기술/웹브라우저객체/js/todo.js
@@ -1,15 +1,19 @@
 const todo = {
   id: 1,
-  data: [], //스케줄 데이터
+  data: [], // 스케줄 데이터
   init() {
-    //저장 값 조회 -> 스케줄 완성
+    // 저장 값 조회 -> 스케줄 완성
     const jsonData = localStorage.getItem("todos");
     const todos = jsonData ? JSON.parse(jsonData) : [];
+    this.data = todos;
+    this.id = todos.length + 1;
 
     const itemsEl = document.querySelector(".items");
+
     for (const item of todos) {
       // Symbol.iterator / 반복자 패턴 / 반복이 필요한 객체
       const liEl = this.getItem(item.title);
+      liEl.dataset.id = item.id;
       itemsEl.appendChild(liEl);
     }
   },
@@ -21,12 +25,13 @@ const todo = {
     const subject = frmRegist.subject.value;
 
     if (!subject.trim()) {
-      //좌우 공백 제거
-      alert("ㄴㄴ");
-      return; //리턴할경우 함수 종료
+      // 좌우 공백 제거
+      alert("할일을 입력하세요.");
+      return;
     }
 
     const itemsEl = document.querySelector(".items");
+    const liEl = this.getItem(subject);
     itemsEl.appendChild(liEl);
 
     frmRegist.subject.value = "";
@@ -40,6 +45,8 @@ const todo = {
     });
 
     this.save();
+
+    liEl.dataset.id = id;
   },
   save() {
     localStorage.setItem("todos", JSON.stringify(this.data));
@@ -52,10 +59,18 @@ const todo = {
     buttonEl.appendChild(document.createTextNode("삭제"));
     liEl.appendChild(buttonEl);
 
-    //스케줄 삭제
+    // 스케줄 삭제
     buttonEl.addEventListener("click", function () {
       const itemsEl = document.querySelector(".items");
       itemsEl.removeChild(liEl);
+
+      //localStorage에 저장된 데이터도 삭제
+      const id = liEl.dataset.id;
+      const index = todo.data.findIndex((item) => item.id == id);
+      if (index !== -1) {
+        todo.data.splice(index, 1);
+        todo.save();
+      }
     });
 
     return liEl;
@@ -63,10 +78,10 @@ const todo = {
 };
 
 window.addEventListener("DOMContentLoaded", function () {
-  todo.init(); //데이터 조회 및 완성
+  todo.init(); // 데이터 조회 및 완성
 
   frmRegist.addEventListener("submit", function (e) {
     e.preventDefault();
-    todo.add(); //스케줄 추가
+    todo.add(); // 스케줄 추가
   });
 });
