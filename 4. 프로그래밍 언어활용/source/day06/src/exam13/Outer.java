670a8d73e1dbd89d941a61e4b298e636ea2be0fc
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-04-22 19:49:03 +0900

    interface 객체 주석

--- 4. 프로그래밍 언어활용/source/day06/src/exam13/Outer.java
+++ 4. 프로그래밍 언어활용/source/day06/src/exam13/Outer.java
@@ -1,16 +1,15 @@
 package exam13;
-/*
+
 public class Outer {
-    /*
-    public Calculator method(int num3) {   //데이터 영역에 num3이 할당됨(상수)
-        Calculator cal = new Calculator() {
-            public int add(int num1, int num2) {
-                return num1 + num2 + num3;
+
+    public Calculator method(int num3) {   //데이터 영역에 num3이 할당됨(상수)   인터페이스 Calculator를 통해 만들 객체의 주소
+        Calculator cal = new Calculator() {              //객체 생성 근데 명확하지 않아서 {} 로 명확하게 만듬
+            public int add(int num1, int num2) {             // 명확한 함수 구현 이 행위를 함으로써 객체가 생성될 조건 완성
+                return num1 + num2 + num3;                 // 반환
             }
         };
 
-        return cal;
+        return cal;   //객체의 주소를 반환함
     }
+}
 
-     */
-//}
