5e66f3b880898c4b343b2f7e019e9587c2c179c8
DESKTOP-KMNUJB9\dkemfdk <dkemfdk4@Naver.com>
2024-04-25 22:39:06 +0900

    no message

--- 4. 프로그래밍 언어활용/source/day05/src/exam02/Ex04.java
+++ 4. 프로그래밍 언어활용/source/day05/src/exam02/Ex04.java
@@ -2,8 +2,14 @@ package exam02;
 
 public class Ex04 {
     public static void main(String[] args) {
-        A ad =  new D();  //D는 A를 상속받음
-        A ac = new C();   //C는 A를 상속받음, ac객체는 C 주소를 가리킴
+        A ad =  new A();  //D는 A를 상속받음
+        A ac = new A();   //C는 A를 상속받음, ac객체는 C 주소를 가리킴
+
+        System.out.println(ad == ac);
+        ad.numA = 20;
+        System.out.println(System.identityHashCode(ad));
+        System.out.println(System.identityHashCode(ac));
+
 
         if (ad instanceof C) {
             C c = (C) ad;
