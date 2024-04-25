04202a4709208c53f6e664ac49a49ebd3cdeb174
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-04-25 19:31:04 +0900

    string_class

---
+++ 4. 프로그래밍 언어활용/source/day09/src/exam03/Ex03.java
@@ -0,0 +1,9 @@
+package exam03;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        String str = "    ";
+        System.out.println(str.isBlank());  // true
+        System.out.println(str.isEmpty());  // false
+    }
+}
---
+++ 4. 프로그래밍 언어활용/source/day09/src/exam03/Ex04.java
@@ -0,0 +1,12 @@
+package exam03;
+
+import java.util.Arrays;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        String fruits = "Apple, Orange, Mango, Melon, Apple";
+        System.out.println(fruits.indexOf("Apple"));
+        System.out.println(fruits.lastIndexOf("Apple"));
+        System.out.println(fruits.indexOf("bababaa"));
+    }
+}
