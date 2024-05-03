6ea5c31fe39171172d42ed51af135623d0686f2b
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-05-03 17:00:59 +0900

    Optional

--- 4. 프로그래밍 언어활용/source/day15/src/exam01/Ex07.java
+++ 4. 프로그래밍 언어활용/source/day15/src/exam01/Ex07.java
@@ -8,6 +8,7 @@ public class Ex07 {
         List<String> fruits = Arrays.asList("사과", "배", "오렌지", "망고", "멜론");
 
         String[] selected = fruits.stream()
+                .peek(System.out::println)
                 .skip(2)
                 .limit(2)
                 .toArray(String[]::new); //i -> new String[i]
