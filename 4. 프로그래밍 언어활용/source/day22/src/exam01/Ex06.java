8974e733517a2c4bc6ff6433c8867d2825f0964f
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-05-16 16:03:17 +0900

    file

--- 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex06.java
+++ 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex06.java
@@ -7,6 +7,8 @@ public class Ex06 {
     public static void main(String[] args) throws IOException, InterruptedException {
         File tmpFile = File.createTempFile("tmp", ".log", new File ("D:/fstudy"));
 
+        tmpFile.deleteOnExit();
+
         Thread.sleep(5000);
         System.out.println("종료!");
     }
