bda83be1408bbfc4c2265b2f92cc584ec0db5324
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-05-22 12:42:37 +0900

    gradle

--- 4. 프로그래밍 언어활용/source/day07/src/exam01/Outer.java
+++ 4. 프로그래밍 언어활용/source/day07/src/exam01/Outer.java
@@ -2,10 +2,11 @@ package exam01;
 
 public class Outer {
     public Calculator method(int num3) {
-       return new Calculator() {
+       Calculator cal = new Calculator() {
             public int add(int num1, int num2) {
                 return num1 + num2 + num3;
             }
         };
+       return cal;
     }
 }
--- 7. SQL 응용/exam02/src/main/java/member/Member.java
+++ 7. SQL 응용/exam02/src/main/java/member/Member.java
@@ -5,12 +5,18 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Data
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+//@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class Member {
+    @NonNull
     private String userId;
+    @NonNull
     private String userNm;
     private String email;
+
+    @ToString.Exclude
     private LocalDateTime regDt;
 
 }
