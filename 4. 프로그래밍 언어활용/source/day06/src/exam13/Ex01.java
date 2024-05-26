e53ada137ff156e3d0ec7f99017e00abc138744a
DESKTOP-KMNUJB9\dkemfdk <dkemfdk4@Naver.com>
2024-05-26 21:56:26 +0900

    no message

--- 4. 프로그래밍 언어활용/source/day06/src/exam13/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day06/src/exam13/Ex01.java
@@ -2,12 +2,9 @@ package exam13;
 
 public class Ex01 {
     public static void main(String[] args) {
-        Outer out = new Outer();              //우선 Outer 클래스를 객체로 생성
-        Calculator cal = out.method(30);         //method를 실행하면  객체의 주소가 반환되는데 그걸 cal에 할당. 참조를 계속함으로써 제거되지 않고 유지.
-        int result = cal.add(10, 20); // 30 + 10 + 20      result라는 변수에 cal 객체의 add메서드 대입 값을 할당
+        Outer out = new Outer();                             //우선 Outer 클래스를 객체로 생성
+        Calculator cal = out.method(30);                  //method를 실행하면  객체의 주소가 반환되는데 그걸 cal에 할당. 참조를 계속함으로써 제거되지 않고 유지.
+        int result = cal.add(10, 20); // 30 + 10 + 20             result라는 변수에 cal 객체의 add메서드 대입 값을 할당
         System.out.println(result);
-
-        int result2 = cal.add(20,30); // 30 + 20 + 30
-        System.out.println(result2);
     }
 }
---
+++ Study/study_inner_class/src/exam01/Ex.java
@@ -0,0 +1,17 @@
+package exam01;
+
+public class Ex {
+    class In { // 인스턴스 내부 클래스
+
+    }
+
+    static class Sin { // 정적 내부 클래스
+
+    }
+
+    public void abc() {
+        class Local { // 지역 내부 클래스
+
+        }
+    }
+}
