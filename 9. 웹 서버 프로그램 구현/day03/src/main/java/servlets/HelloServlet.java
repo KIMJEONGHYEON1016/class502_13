01ba8f5f702bc9565b36d5354de0385b219f2413
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-06-11 15:56:04 +0900

    servlet

--- 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
+++ 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,8 +9,35 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
+    /*
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init()");
+    }
+    */
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        // HelloServlet만 조회 가능
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s%n", key1, key2);
+
+
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy()");
+    }
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("안녕하세요");
+        System.out.println("doGet");
+
+        // 모든 서블릿이 조회 가능
+        String common1 = req.getServletContext().getInitParameter("common1");
+        String common2 = req.getServletContext().getInitParameter("common2");
+        System.out.println(common1 + "," + common2);
     }
 }
---
+++ 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/JoinServlet.java
@@ -0,0 +1,17 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class JoinServlet extends HelloServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
