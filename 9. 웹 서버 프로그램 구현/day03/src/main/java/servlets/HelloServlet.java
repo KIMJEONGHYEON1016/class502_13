2c09620395fef1dd882895fd0677f8032b59be23
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-06-11 14:04:53 +0900

    doget

--- 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
+++ 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
@@ -1,4 +1,15 @@
 package servlets;
 
-public class HelloServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("안녕하세요");
+    }
 }
