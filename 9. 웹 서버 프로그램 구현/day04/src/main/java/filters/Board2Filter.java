45ebac79b58c1bc78a414e22ed0e44e0b2a74ad6
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-06-12 15:15:53 +0900

    wrapper annotation

--- 9. 웹 서버 프로그램 구현/day04/src/main/java/filters/Board2Filter.java
+++ 9. 웹 서버 프로그램 구현/day04/src/main/java/filters/Board2Filter.java
@@ -2,9 +2,11 @@ package filters;
 
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 
 import java.io.IOException;
 
+@WebFilter("/board2")
 public class Board2Filter implements Filter {
 
     @Override
