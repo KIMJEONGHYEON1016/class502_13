f39a618dd619296d58e33baf0d7451b0fdbe63e7
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-06-14 17:49:57 +0900

    core_library

--- 9. 웹 서버 프로그램 구현/day05/src/main/java/board/controllers/BoardListController.java
+++ 9. 웹 서버 프로그램 구현/day05/src/main/java/board/controllers/BoardListController.java
@@ -10,16 +10,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet("/board/list/*")
 public class BoardListController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         BoardInfoService service = new BoardInfoService();
         List<BoardData> items = service.getList();
 
         req.setAttribute("items", items);
+
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>이전 출력 테스트</h1>");
 
         RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/board/list.jsp");
         rd.forward(req, resp);
