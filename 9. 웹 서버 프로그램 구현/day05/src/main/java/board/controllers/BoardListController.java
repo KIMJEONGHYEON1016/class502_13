00daa8bb3ea811565f5b47705dfe919c1a3c6f88
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-06-18 12:38:35 +0900

    custom_action_layout

--- 9. 웹 서버 프로그램 구현/day05/src/main/java/board/controllers/BoardListController.java
+++ 9. 웹 서버 프로그램 구현/day05/src/main/java/board/controllers/BoardListController.java
@@ -10,22 +10,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet("/board/list/*")
 public class BoardListController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         BoardInfoService service = new BoardInfoService();
         List<BoardData> items = service.getList();
 
         req.setAttribute("items", items);
 
-        resp.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.println("<h1>이전 출력 테스트</h1>");
+        req.setAttribute("addCss", new String[] { "board/style", "board/list"});
+        req.setAttribute("addScript", List.of("board/common", "board/list"));
 
+        // 속성 설정은 RequestDispatcher 상단에 정의
+        // 출력 완료 되면 속성은 반영 X
         RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/board/list.jsp");
         rd.forward(req, resp);
     }
