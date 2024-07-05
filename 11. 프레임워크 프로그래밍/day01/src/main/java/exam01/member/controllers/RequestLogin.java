6893135dc3cb6c4697e8be1723fe69c2b216a23f
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-07-05 16:45:02 +0900

    final

---
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/controllers/RequestLogin.java
@@ -0,0 +1,13 @@
+package exam01.member.controllers;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class RequestLogin {
+    private String email;
+    private String password;
+}
