57957e916d334f89f7beb54329a84996d4ea6d7e
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-07-16 16:01:16 +0900

    file

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/exceptions/CommonException.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/exceptions/CommonException.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 public class CommonException extends RuntimeException {
 
     private HttpStatus status;
+
     public CommonException(String message) {
         this(message, HttpStatus.INTERNAL_SERVER_ERROR); // 500
     }
--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,5 +1,7 @@
 package org.choongang.member.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,7 @@ import org.choongang.member.services.LoginService;
 import org.choongang.member.validators.JoinValidator;
 import org.choongang.member.validators.LoginValidator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
@@ -108,6 +111,15 @@ public class MemberController {
 
         log.info("email:{}, email2:{}", email, email2);
     }
+
+     /*
+    @ExceptionHandler(Exception.class)
+    public String errorHandler(Exception e, HttpServletRequest request, HttpServletResponse response, Model model) {
+        e.printStackTrace();
+        log.info("MemberController에서 유입");
+        return "error/common";
+    }
+    */
 
 
     /*
