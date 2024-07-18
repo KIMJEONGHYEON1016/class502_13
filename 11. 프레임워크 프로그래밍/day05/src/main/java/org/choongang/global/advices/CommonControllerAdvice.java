0f26a7785f85aa0f281bc59b5e192eec29238d7c
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-07-18 12:07:01 +0900

    error json

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
@@ -6,12 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.choongang.global.exceptions.CommonException;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
-@ControllerAdvice("org.choongang")
+//@ControllerAdvice("org.choongang")
 public class CommonControllerAdvice {
 
     @ExceptionHandler(Exception.class)
@@ -21,14 +20,13 @@ public class CommonControllerAdvice {
 
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 500
         if (e instanceof CommonException commonException) {
-            //CommonException commonException = (CommonException) e;
+            // CommonException commonException = (CommonException) e;
             status = commonException.getStatus();
         }
 
         ModelAndView mv = new ModelAndView();
-        mv.setStatus(status);
-
         mv.setViewName("error/common");
+        mv.setStatus(status);
 
         return mv;
     }
