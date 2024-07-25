f2de61d3a4c6dde6bcda7bf3bc65bcbccbc1be80
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-07-25 11:38:20 +0900

    연관관계매핑

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex10.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex10.java
@@ -2,10 +2,11 @@ package org.choongang.jpa_study;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import org.choongang.board.entities.BoardData;
 import org.choongang.board.repositories.BoardDataRepository;
 import org.choongang.member.constants.Authority;
 import org.choongang.member.entities.Member;
+import org.choongang.member.entities.MemberProfile;
+import org.choongang.member.repositories.MemberProfileRepository;
 import org.choongang.member.repositories.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,56 +15,48 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.IntStream;
-
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
-public class Ex09 {
-
+public class Ex10 {
     @Autowired
     private MemberRepository memberRepository;
 
     @Autowired
     private BoardDataRepository boardDataRepository;
 
+    @Autowired
+    private MemberProfileRepository profileRepository;
+
     @PersistenceContext
     private EntityManager em;
 
     @BeforeEach
     void init() {
+        MemberProfile profile = MemberProfile.builder()
+                .profileImage("이미지")
+                .status("상태")
+                .build();
+
+        profileRepository.saveAndFlush(profile);
+
         Member member = Member.builder()
                 .email("user01@test.org")
                 .password("12345678")
                 .userName("사용자01")
+                .profile(profile)
                 .authority(Authority.USER)
                 .build();
 
         memberRepository.saveAndFlush(member);
 
-        List<BoardData> items = IntStream.rangeClosed(1, 10).mapToObj(i -> BoardData.builder()
-                .subject("제목" + i)
-                .content("내용" + i)
-                .member(member)
-                .build()).toList();
-
-        boardDataRepository.saveAllAndFlush(items);
         em.clear();
     }
 
     @Test
     void test1() {
-        BoardData item = boardDataRepository.findById(1L).orElse(null);
-
-        Member member = item.getMember();
-        System.out.println(member);
-    }
-
-    @Test
-    void test2() {
         Member member = memberRepository.findById(1L).orElse(null);
-        List<BoardData> items = member.getItems();
-        items.forEach(System.out::println);
+        MemberProfile profile = member.getProfile();
+        System.out.println(profile);
     }
 }
